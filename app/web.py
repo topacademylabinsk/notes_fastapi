61906d3c38ff58a28c60b6e3a523d69bca7ac5ee
anatoliy <anatoliy@example.com>
2025-07-18 16:20:09 +0300

    Changes to be committed: 	new file:   app/static/styles/base.css 	new file:   app/static/styles/index.css 	new file:   app/templates/base.html 	modified:   app/templates/index.html 	modified:   app/web.py 	modified:   main.py

--- app/web.py
+++ app/web.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 templates = Jinja2Templates("./app/templates")
+app.mount("/styles", StaticFiles(directory="./app/static/styles"), name="styles")
+app.mount("/images", StaticFiles(directory="./app/static/images"), name="images")
 
 
 @app.get("/")
