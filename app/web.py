09e30d4f0351119a51dae7d50c370c6a07497224
anatoliy <anatoliy@example.com>
2025-07-21 17:26:18 +0300

    Changes to be committed: 	new file:   app/static/styles/auth.css 	modified:   app/static/styles/base.css 	new file:   app/static/styles/create_note.css 	modified:   app/static/styles/index.css 	new file:   app/static/styles/notes.css 	new file:   app/templates/auth.html 	modified:   app/templates/base.html 	new file:   app/templates/create_note.html 	modified:   app/templates/index.html 	new file:   app/templates/notes.html 	modified:   app/web.py

--- app/web.py
+++ app/web.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, status
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
+from fastapi import responses
 
 app = FastAPI()
 templates = Jinja2Templates("./app/templates")
@@ -9,5 +11,23 @@ app.mount("/images", StaticFiles(directory="./app/static/images"), name="images"
 
 
 @app.get("/")
-async def root(request: Request):
+async def get_root(request: Request):
     return templates.TemplateResponse(request=request, name="index.html")
+
+@app.get("/auth")
+async def get_auth(request: Request):
+    return templates.TemplateResponse(status_code=302, request=request, name="auth.html")
+
+@app.post("/auth")
+async def post_auth(request: Request):
+    response = responses.Response(headers={"HX-Redirect": "/notes"})
+    response.set_cookie("test", "test")
+    return response
+
+@app.get("/create_note")
+async def get_create_note(request: Request):
+    return templates.TemplateResponse(request=request, name="create_note.html")
+
+@app.get("/notes")
+async def get_notes(request: Request):
+    return templates.TemplateResponse(request=request, name="notes.html")
