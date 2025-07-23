a4e846319a4faf2bff03dc5daafd0aa953c64f71
anatoliy <anatoliy@example.com>
2025-07-23 16:51:45 +0300

    Changes to be committed: 	modified:   app/static/styles/auth.css 	modified:   app/static/styles/index.css 	modified:   app/templates/auth.html 	modified:   app/templates/index.html 	new file:   app/templates/login_error.html 	new file:   app/templates/password_error.html 	modified:   app/web.py

--- app/web.py
+++ app/web.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI, Request, status
+import json
+
+from fastapi import FastAPI, Request, responses
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import RedirectResponse
-from fastapi import responses
 
 app = FastAPI()
 templates = Jinja2Templates("./app/templates")
@@ -14,19 +14,45 @@ app.mount("/images", StaticFiles(directory="./app/static/images"), name="images"
 async def get_root(request: Request):
     return templates.TemplateResponse(request=request, name="index.html")
 
+
 @app.get("/auth")
 async def get_auth(request: Request):
-    return templates.TemplateResponse(status_code=302, request=request, name="auth.html")
+    return templates.TemplateResponse(
+        status_code=302, request=request, name="auth.html"
+    )
+
 
 @app.post("/auth")
 async def post_auth(request: Request):
-    response = responses.Response(headers={"HX-Redirect": "/notes"})
-    response.set_cookie("test", "test")
+    test_login = "admin"
+    test_password = "password"
+
+    body = await request.body()
+    data = json.loads(body)
+
+    if data["login"] == test_login:
+        if data["password"] == test_password:
+            response = responses.Response(headers={"HX-Redirect": "/notes"})
+            response.set_cookie("test", "test")
+        else:
+            response = templates.TemplateResponse(
+                request=request, name="password_error.html"
+            )
+    else:
+        response = templates.TemplateResponse(request=request, name="login_error.html")
     return response
+
 
 @app.get("/create_note")
 async def get_create_note(request: Request):
-    return templates.TemplateResponse(request=request, name="create_note.html")
+    try:
+        if request.cookies["test"] == "test":
+            return templates.TemplateResponse(request=request, name="create_note.html")
+        else:
+            return responses.HTMLResponse("Неправильные куки")
+    except Exception:
+        return responses.HTMLResponse("Неправильные куки")
+
 
 @app.get("/notes")
 async def get_notes(request: Request):
